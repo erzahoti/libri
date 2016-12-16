4280c9f2c97e3525c3aa898cda199d69beda0974
erza96 <erzahoti96@gmail.com>
2016-12-17 00:45:36 +0100

    test

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
 </div><br><br>
 
 <footer class="container-fluid text-center">
-  <p>Online Store Copyright</p>
+  <p>Online Store</p>
   <form class="form-inline">Get deals:
     <input type="email" class="form-control" size="50" placeholder="Email Address">
     <button type="button" class="btn btn-danger">Sign Up</button>
